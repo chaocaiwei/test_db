5823affd4f191f04ea1a50749505d0b6a5a65b35
weichaocai <ccsobj@163.com>
2023-07-02 09:39:56 +0800

    update diceloss

--- decoders/loss.py
+++ decoders/loss.py
@@ -4,26 +4,38 @@ import numpy as np
 
 
 class DiceLoss(nn.Module):
-
+    '''
+    Loss function from https://arxiv.org/abs/1707.03237,
+    where iou computation is introduced heatmap manner to measure the
+    diversity bwtween tow heatmaps.
+    '''
     def __init__(self, eps=1e-6):
         super(DiceLoss, self).__init__()
         self.eps = eps
 
-    def forward(self, pred: torch.Tensor, gt: torch.Tensor, mask: torch.Tensor, weights=None):
+    def forward(self, pred: torch.Tensor, gt, mask, weights=None):
+        '''
+        pred: one or two heatmaps of shape (N, 1, H, W),
+            the losses of tow heatmaps are added together.
+        gt: (N, 1, H, W)
+        mask: (N, H, W)
+        '''
         assert pred.dim() == 4, pred.dim()
+        return self._compute(pred, gt, mask, weights)
 
-        pred = pred[:, 0, :, :]
-        gt = gt[:, 0, :, :]
+    def _compute(self, pred, gt, mask, weights):
+        if pred.dim() == 4:
+            pred = pred[:, 0, :, :]
+            gt = gt[:, 0, :, :]
         assert pred.shape == gt.shape
         assert pred.shape == mask.shape
-
-        if weights is None:
+        if weights is not None:
             assert weights.shape == mask.shape
             mask = weights * mask
 
         intersection = (pred * gt * mask).sum()
         union = (pred * mask).sum() + (gt * mask).sum() + self.eps
-        loss = 1 - 2.0 / intersection / union
+        loss = 1 - 2.0 * intersection / union
         assert loss <= 1
         return loss
 
